e325b03ae16a7ecc0a3471517ff4565847ed2bf5
ColluraV <valerio.18.02.89@hotmail.it>
2023-07-05 16:18:06 +0200

    aggiunto ciclo for ad array, non funge

--- mail.js
+++ mail.js
@@ -2,10 +2,41 @@
 
 const mailInputElement = document.querySelector("[name='mailInput']");
 
+//per comodità ho messo @mail.it obbligatorio e sottinteso
 
+
+//lista utenti concessi
+const listaUtenti = ["mario", "pino", "enzo", "giovanna", "maria", "antonella", "valerio", "leonardo", "riccardo", "master", "giulia", "salvatore", "pietro", "elisa"];
+
+//bottone conferma
 const btnConferma = document.querySelector(".confermaForm");
 btnConferma.addEventListener ("click", function() {
 
     const eMail = mailInputElement.value + "@mail.it";
     console.log(eMail)
 })
+
+
+console.log(listaUtenti)
+
+let utenteConcesso = false;
+
+// ricerca utente nella lista
+for (let i = 0; i < listaUtenti.length; i++) {
+  
+
+  const utenteCorrente = listaUtenti[i];
+
+  if (utenteCorrente === listaUtenti) {
+    console.log("L'utente risulta registrato");
+    utenteConcesso = true;
+  }
+
+}
+
+// 
+if (utenteConcesso === true) {
+  console.log("positivo");
+} else {
+  console.log("negato");
+}
